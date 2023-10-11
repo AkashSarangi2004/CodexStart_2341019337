3b99b9935bc85fa099d5e9664e356fd0a233c557
AkashSarangi2004 <87138586+AkashSarangi2004@users.noreply.github.com>
2023-10-11 15:00:34 +0530

    Q1

---
+++ Q1_1068.py
@@ -0,0 +1,17 @@
+"""
+ * Name: Akash Sarangi
+ * Reg No: 2341019337
+ * PS LINK: https://cses.fi/problemset/task/1068/
+"""
+
+n=int(input())
+print(n, end=" ")
+while n!=1:
+    if n%2==0:
+        n=n//2
+        
+    else:
+        n=(n*3)+1
+    
+    print(n, end=" ")    
+
