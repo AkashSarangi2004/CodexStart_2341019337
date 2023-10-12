0d2eaa696ddfb70e832b8ef698feb746a805ede3
AkashSarangi2004 <87138586+AkashSarangi2004@users.noreply.github.com>
2023-10-13 00:23:08 +0530

    Q8

---
+++ Q8_1623.py
@@ -0,0 +1,14 @@
+"""
+ * Name: Akash Sarangi
+ * Reg No: 2341019337
+ * PS LINK: https://cses.fi/problemset/task/1623/
+"""
+
+n = int(input())
+
+w= list(map(int, input().split()))
+def min_difference(index, a, b):
+    if index >= len(w):
+        return abs(a - b)
+    return min(min_difference(index + 1, a + w[index], b), min_difference(index + 1, a, b + w[index]))
+print(min_difference(0, 0, 0))
