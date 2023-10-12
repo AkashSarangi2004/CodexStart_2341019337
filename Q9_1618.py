8e14e36bc3754bb7173dde0e680c9e5f5e0c93c3
AkashSarangi2004 <87138586+AkashSarangi2004@users.noreply.github.com>
2023-10-13 00:31:08 +0530

    Q9

---
+++ Q9_1618.py
@@ -0,0 +1,12 @@
+"""
+ * Name: Akash Sarangi
+ * Reg No: 2341019337
+ * PS LINK: https://cses.fi/problemset/task/1618/
+"""
+n = int(input())
+c = 0
+i = 5
+while(n/i >= 1):
+    c += int(n/i)
+    i *= 5
+print(c)
