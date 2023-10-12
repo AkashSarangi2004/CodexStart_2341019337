3dae8693a28079159539b8104d922ebcb674ea19
AkashSarangi2004 <87138586+AkashSarangi2004@users.noreply.github.com>
2023-10-13 01:15:49 +0530

    Q3

--- Q3_1070.py
+++ Q3_1070.py
@@ -3,21 +3,17 @@
  * Reg No: 2341019337
  * PS LINK: https://cses.fi/problemset/task/1070/
 """
-
 n = int(input())
-
-if n == 1:
+if n==1:
     print(1)
 elif n <= 3:
     print("NO SOLUTION")
 else:
-    # Print all odd numbers in decreasing order, then all even numbers in decreasing order
-    even = []
-    odd = []
+    even=[]
+    odd=[]
     for i in range(n, 0, -1):
-        if i % 2 == 0:
+        if i%2==0:
             even.append(i)
         else:
             odd.append(i)
-
     print(*(odd + even))
